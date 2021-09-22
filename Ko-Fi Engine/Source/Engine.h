f535d939080bf37d43e209e84302bb137ccbd4cd
Omicrxn <alexavila0128@gmail.com>
2021-09-22 10:17:43 +0200

    Making the base (There are errors)

---
+++ Ko-Fi Engine/Source/Engine.h
@@ -0,0 +1,109 @@
+#pragma once
+#ifndef __ENGINE_H__
+#define __ENGINE_H__
+
+#include "Module.h"
+#include "PerfTimer.h"
+#include "Timer.h"
+
+#include "List.h"
+
+// Modules
+class FileSystem
+class Window;
+class Input;
+class SceneManager;
+
+class Renderer;
+class Camera;
+class Editor;
+class Resources;
+class Shaders;
+
+class GameObject;
+
+class KoFiEngine
+{
+public:
+
+	// Constructor
+	KoFiEngine(int argc, char* args[]);
+
+	// Destructor
+	virtual ~KoFiEngine();
+
+	// Called before render is available
+	bool Awake();
+
+	// Called before the first frame
+	bool Start();
+
+	// Called each loop iteration
+	bool Update();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Exposing some properties for reading
+	int GetArgc() const;
+
+	const char* GetArgv(int index) const;
+	const char* GetTitle() const;
+	const char* GetOrganization() const;
+
+private:
+	// Add a new module to handle
+	void AddModule(Module* module);
+
+	// Call modules before each loop iteration
+	void PrepareUpdate();
+
+	// Call modules before each loop iteration
+	void FinishUpdate();
+
+	// Call modules before each loop iteration
+	bool PreUpdate();
+
+	// Call modules on each loop iteration
+	bool DoUpdate();
+
+	// Call modules after each loop iteration
+	bool PostUpdate();
+
+public:
+	//Modules
+	FileSystem* fileSystem = nullptr;
+	Window* window = nullptr;
+	Input* input = nullptr;
+	SceneManager* sceneManager = nullptr;
+
+	Renderer* renderer = nullptr;
+	Camera* camera = nullptr;
+	Editor* editor = nullptr;
+	Resources* resources = nullptr;
+	Shaders* shaders = nullptr;
+
+private:
+	int argc;
+	char** args;
+	SString title;
+	SString organization;
+
+	List<Module*> modules;
+
+	PerfTimer ptimer;
+	uint64 frameCount = 0;
+
+	Timer startupTime;
+	Timer frameTime;
+	Timer lastSecFrameTime;
+	uint32 lastSecFrameCount = 0;
+	uint32 prevLastSecFrameCount = 0;
+	float dt = 0.0f;
+
+	int	cappedMs = -1;
+
+
+};
+
+#endif //__ENGINE_H__
---
+++ Ko-Fi Engine/Source/Main.cpp
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+
+#include "Defs.h"
+#include "Log.h"
+
+#include "Engine.h"
+
+
+
+enum MainState
+{
+	CREATE = 1,
+	AWAKE,
+	START,
+	LOOP,
+	CLEAN,
+	FAIL,
+	EXIT
+};
+
+int main(int argc, char* args[])
+{
+	Engine* engine = NULL;
+	LOG("Engine starting ...");
+
+	MainState state = CREATE;
+	int result = EXIT_FAILURE;
+
+	while (state != EXIT)
+	{
+		switch (state)
+		{
+			// Allocate the engine --------------------------------------------
+		case CREATE:
+			LOG("CREATION PHASE ===============================");
+			if (engine != NULL)
+				state = AWAKE;
+			else
+				state = FAIL;
+
+			break;
+
+			// Awake all modules -----------------------------------------------
+		case AWAKE:
+			LOG("AWAKE PHASE ===============================");
+			if (engine->Awake() == true)
+				state = START;
+			else
+			{
+				LOG("ERROR: Awake failed");
+				state = FAIL;
+			}
+
+			break;
+
+			// Call all modules before first frame  ----------------------------
+		case START:
+			LOG("START PHASE ===============================");
+			if (engine->Start() == true)
+			{
+				state = LOOP;
+				LOG("UPDATE PHASE ===============================");
+			}
+			else
+			{
+				state = FAIL;
+				LOG("ERROR: Start failed");
+			}
+			break;
+
+			// Loop all modules until we are asked to leave ---------------------
+		case LOOP:
+			if (engine->Update() == false)
+				state = CLEAN;
+			break;
+
+			// Cleanup allocated memory -----------------------------------------
+		case CLEAN:
+			LOG("CLEANUP PHASE ===============================");
+			if (engine->CleanUp() == true)
+			{
+				RELEASE(app);
+				result = EXIT_SUCCESS;
+				state = EXIT;
+			}
+			else
+				state = FAIL;
+
+			break;
+
+			// Exit with errors and shame ---------------------------------------
+		case FAIL:
+			LOG("Exiting with errors :(");
+			result = EXIT_FAILURE;
+			state = EXIT;
+			break;
+		}
+	}
+
+	LOG("... Bye! :)\n");
+
+	// Dump memory leaks
+	return result;
+}
