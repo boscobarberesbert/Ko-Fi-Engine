6cbe0c07a36bfdd026d9d6fa00636ba45aea975b
Lladruc37 <sergi@kromatika.com>
2022-03-12 11:53:01 +0100

    fixed a crash

--- Ko-Fi Engine/Source/Renderer3D.cpp
+++ Ko-Fi Engine/Source/Renderer3D.cpp
@@ -317,13 +317,13 @@ void ParticleRenderer::Render()
 	glPopMatrix();
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_NORMAL_ARRAY, 0);
-	glBindBuffer(GL_TEXTURE_COORD_ARRAY, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glBindBuffer(GL_NORMAL_ARRAY, 0);
+	//glBindBuffer(GL_TEXTURE_COORD_ARRAY, 0);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	glDisableClientState(GL_VERTEX_ARRAY);
-	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	//glDisableClientState(GL_VERTEX_ARRAY);
+	//glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 
 	glDisable(GL_BLEND);
 	glDisable(GL_ALPHA_TEST);
