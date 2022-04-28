0af5e9667f077283c07e9c0e2f771e632fb44ddb
Montuuh <dmontufo@gmail.com>
2022-04-28 17:04:38 +0200

    Added win trigger script

---
+++ Ko-Fi Engine/Ko-Fi/Assets/Scripts/Triggers/WinTrigger.lua
@@ -0,0 +1,15 @@
+sceneName = "default_level"
+
+local sceneNameIVT = INSPECTOR_VARIABLE_TYPE.INSPECTOR_STRING
+sceneNameIV = InspectorVariable.new("sceneName", sceneNameIVT, sceneName)
+NewVariable(sceneNameIV)
+
+------------------ Collisions --------------------
+function OnTriggerEnter(go)
+	if (go.tag == Tag.PLAYER) then
+		SetChangeSceneAndName(true, name)
+	end
+end
+--------------------------------------------------
+
+print("WinTrigger.lua compiled succesfully")
