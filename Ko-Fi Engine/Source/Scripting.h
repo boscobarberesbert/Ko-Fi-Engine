640f6e27a343881c4741b5c7b4e96262ca40142a
Montuuh <dmontufo@gmail.com>
2022-06-12 16:54:42 +0200

    Function to change text color to scripting

--- Ko-Fi Engine/Source/Scripting.h
+++ Ko-Fi Engine/Source/Scripting.h
@@ -286,7 +286,8 @@ public:
 			"GetTextValue", &C_Text::GetTextValue,
 			"SetTextValue", &C_Text::SetTextValue,
 			"GetTextOpacity", &C_Text::GetOpacity,
-			"SetTextOpacity", &C_Text::SetOpacity);
+			"SetTextOpacity", &C_Text::SetOpacity,
+			"SetTextColor", &C_Text::SetColorRGBA);
 
 		// Component Image
 		lua.new_usertype<C_Image>("C_Image",
