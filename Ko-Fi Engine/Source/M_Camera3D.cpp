0845d6a05a8d763f46f0418959826fcad3f59ba6
tomascarreras1000 <tomascarreras1000@gmail.com>
2022-06-13 01:05:39 +0200

    Mouse picking ignores unactive GOs

--- Ko-Fi Engine/Source/M_Camera3D.cpp
+++ Ko-Fi Engine/Source/M_Camera3D.cpp
@@ -403,7 +403,7 @@ GameObject* M_Camera3D::MousePicking(const bool& isRightButton)
 	for (size_t i = 0; i < sceneGameObjects.size(); i++)
 	{
 		C_Mesh* m = sceneGameObjects[i]->GetComponent<C_Mesh>();
-		if (m != nullptr)
+		if (m != nullptr && sceneGameObjects[i]->active)
 		{
 			bool hit = newRay.Intersects(m->GetGlobalAABB());
 
